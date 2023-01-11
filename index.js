f4145ea0a0ac35b9e0f7c5d5b6059fcbd03c6dde
Bhargavi tanneru <59660244+bhargavitanneru@users.noreply.github.com>
2023-01-11 05:45:38 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-exports.handler = function (event, context) {
-	context.succeed('hello world');
+const { MongoClient } = require("mongodb");
+
+exports.handler = async (event) => {
+	const filter = {
+		'orderNumber': '123'
+	};
+	const client = await MongoClient.connect(
+		'mongodb+srv://btanneru:Bhani4chinni@dev0.aar2e.mongodb.net/test?authSource=admin&replicaSet=dev0-shard-0&readPreference=primary&ssl=true',
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	);
+	const coll = client.db('smartCell_Commons').collection('order');
+	const cursor = coll.find(filter);
+	const result = await cursor.toArray();
+	console.log(`result:: ${result}`);
+	await client.close();
+	return result;
 };
